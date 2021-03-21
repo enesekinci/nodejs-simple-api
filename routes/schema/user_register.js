8b7781709dc7e51c16e5fc36097c13e129e34d1e
enesekinci <enes.eknc.96@gmail.com>
2021-03-21 16:10:22 +0300

    user routes added and schema created

---
+++ routes/schema/user_register.js
@@ -0,0 +1,10 @@
+module.exports = {
+    body: {
+        type: 'object',
+        properties: {
+            name: { type: 'string' }
+        },
+        required: ['name']
+    }
+}
+
--- routes/user.js
+++ routes/user.js
@@ -1,19 +1,23 @@
-async function routes(fastify, options) {
+const { UserController } = require("../controller/user_controller");
+const Joi = require('joi')
+const config = require('dotenv').config().parsed
+const registerSchema = require('./schema/user_register')
 
-    fastify.post('/register', async (request, response) => {
-        response.send({ hello: 'world test', request:request.body })
+module.exports = async function routes(fastify, options, next) {
+
+    fastify.post('/register', { schema: registerSchema }, async (request, response, next) => {
+        response.send({ hello: 'world test', request: request.body })
     })
 
-    fastify.post('/login', async (request, response) => {
-        response.send({ bye: 'good bye' })  
+    fastify.post('/login', async (request, response, next) => {
+        response.send({ bye: 'good bye' })
     })
 
-    fastify.post('/generate-membership-token', async (request, reply) => {
+    fastify.post('/generate-membership-token', UserController.generateMemberShipTokken);
+
+    fastify.get('/check-membership-token', { preValidation: [fastify.jwt_verify] }, async (request, reply) => {
         return null
     });
-    fastify.post('/check-membership-token', async (request, reply) => {
-        return null
-    });
+
+    next()
 }
-
-module.exports = routes
