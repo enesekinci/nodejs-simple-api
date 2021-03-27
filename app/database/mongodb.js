6b66540d80767c716b40a9b8d8eb6d26d27c1fe6
enesekinci <enes.eknc.96@gmail.com>
2021-03-27 21:23:10 +0300

    mongo connect is modified

--- app/database/mongodb.js
+++ app/database/mongodb.js
@@ -1,6 +1,10 @@
 const config = require('dotenv').config().parsed
 const mongoose = require('mongoose');
 
-mongoose.connect(config.MONGO_DB_HOST + ':' + config.MONGO_DB_PORT, { useCreateIndex: true, useUnifiedTopology: true, useNewUrlParser: true })
+mongoose.connect(config.MONGO_DB_HOST + ':' + config.MONGO_DB_PORT + '/' + config.MONGO_DB_DATABASE, {
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+})
     .then(() => console.log('db is running'))
     .catch(() => console.log('db is not running'));
