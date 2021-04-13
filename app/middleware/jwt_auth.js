ae3e145f7e29cc931758eaa423beab056af3547e
enesekinci <enes.eknc.96@gmail.com>
2021-04-13 18:35:35 +0300

    jwt auth fixed

--- app/middleware/jwt_auth.js
+++ app/middleware/jwt_auth.js
@@ -1,21 +1,20 @@
 const { JwtHelper } = require("../helper/jwt_helper");
 const createHttpError = require('http-errors')
 const fastifyPlugin = require('fastify-plugin')
-
+const User = require('../model/user')
 
 module.exports = fastifyPlugin(async (fastify) => {
     fastify.decorate('JwtAuth', async (request, response) => {
         const getToken = request.headers.authorization;
-        if (!getToken) throw createHttpError(403, 'Token Bilgisi Yok!');
+        if (!getToken) throw createHttpError(403, 'Token Bilgisi Yok!')
 
-        const token = getToken.replace('Bearer ', '');
-        const result = await JwtHelper.JWTDecode(token);
-
+        const token = getToken.replace('Bearer ', '')
+        const result = await JwtHelper.JWTDecode(token)
         if (!result) throw createHttpError(403, "Token Süresi Dolmuş!")
 
-        const user = await User.findById({ email: result.user.email })
-        if (user && user.status != 0) request.user = user
+        const user = await User.findOne({ email: result.data.email })
 
-        throw createHttpError(403, 'Kullanıcı Bulunamadı.')
+        if (user && user.status != 0) request.user = user
+        else throw createHttpError(403, 'Kullanıcı Bulunamadı.')
     })
 })
